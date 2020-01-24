3cefb8ba4f797fc53dcc346faf7e5497720c6cd3
becauseIknowNothing <lovish.madhu.2404@gmail.com>
2020-01-24 18:37:12 +0530

    Added CSS/Bootstrap

--- blog.py
+++ blog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+	{
+		'author' : 'Lovish',
+		'title' : 'Blog post 1',
+		'content': 'First Post content',
+		'date_posted': 'Jan 24, 2019'
+	},
+	{
+		'author' : 'Komal',
+		'title' : 'Blog post 2',
+		'content': 'Second Post content',
+		'date_posted': 'Jan 23, 2019'
+	}
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "This is the home page"
+	return render_template('home.html', posts = posts, title = "home")
 
 @app.route("/about")
 def about():
-	return "This is the about page"
+	return render_template('about.html', title = "about")
 
 if __name__=='__main__':
 	app.run(debug=True)
